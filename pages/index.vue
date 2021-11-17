09520d05b8b1487f6f4986072de0debdc28f5a93
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2021-11-17 01:23:16 -0300

    Criação da página de carrinho

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="container">
-
     <button v-on:click="saySomething" class="btn btn-primary">Say</button>
-
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { market } from "@/store";
 
 export default Vue.extend({
+  asyncData() {
+    console.log(market.$products);
+  },
   methods: {
-    saySomething: function() {
-      this.$toast.success("Teste!");
+    saySomething: function () {
+      this.$toast.success(`${market.$products}`);
     }
   }
 });
---
+++ pages/shoppingbag/index.vue
@@ -0,0 +1,244 @@
+<template>
+  <div class="container">
+    <section>
+      <ul class="products-in-bag">
+        <li class="product">
+          <div class="product-image-box">
+            <img src="@/assets/images/hamburguer.jpg" class="product-image" />
+          </div>
+          <div class="product-information-box">
+            <div class="product-name">Hambúrguer de Sirí</div>
+            <div class="product-description">
+              Dois hambúrgueres, alface, tomate, queijo e molho especial, cebola
+              e picles num pão com gergelim. O sabor de McDonald's duplamente
+              delicioso.
+            </div>
+          </div>
+          <div class="product-property-box">
+            <div class="product-quantity">
+              <button class="btn btn-primary">-</button>
+              <button class="btn btn-outline-primary info-quant" disabled>
+                0
+              </button>
+              <button class="btn btn-primary">+</button>
+            </div>
+
+            <div class="product-remove">
+              <button
+                type="button"
+                class="btn btn-outline-danger btn-md btn-block rounded-pill"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <aside class="grand-total">
+      <div class="product-information">
+        <div class="info">
+          <h1 class="product-name">Hamburguer de Sirí</h1>
+          <p class="product-description">
+            Dois hambúrgueres, alface, tomate, queijo e molho especial, cebola e
+            picles num pão com gergelim. O sabor de McDonald's duplamente
+            delicioso.
+          </p>
+        </div>
+
+        <div class="value">
+          <div class="product-total-quantity"><strong>Quantidade do produto</strong>: 0</div>
+          <div class="product-price"><strong>Valor do produto</strong>: R$22.50</div>
+        </div>
+      </div>
+
+      <div class="total-value">
+        O total da compra é de R$68.80!
+        <div class="options">
+          <button class="btn btn-success btn-block">Finalizar Pedido</button>
+          <button class="btn btn-danger btn-blick">Limpar Carrinho</button>
+        </div>
+      </div>
+    </aside>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  font-family: sans-serif;
+  min-height: 500px;
+  margin: 25px auto;
+
+  grid-template-columns: 7fr 4fr;
+  grid-template-areas: "section aside";
+
+  gap: 1rem;
+  display: grid;
+}
+
+section {
+  border-radius: 25px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16), 0 2px 10px rgba(0, 0, 0, 0.12);
+  background-color: whitesmoke;
+  grid-template-columns: 1fr;
+
+  grid-area: section;
+
+  .products-in-bag {
+    padding: 15px;
+    margin: 0;
+
+    list-style: none;
+    margin-top: 25px;
+
+    justify-content: center;
+    align-content: center;
+
+    gap: 0.5rem;
+    display: grid;
+
+    .product {
+      grid-template-columns: 4fr 6fr 4fr;
+      grid-auto-rows: 200px;
+      grid-auto-flow: column;
+      display: grid;
+      gap: 1rem;
+
+      .product-image-box {
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16),
+          0 2px 10px rgba(0, 0, 0, 0.12);
+        justify-content: center;
+        align-content: center;
+        border-radius: 25px;
+
+        display: grid;
+
+        .product-image {
+          width: 80%;
+          height: auto;
+          border-radius: 50%;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16),
+            0 2px 10px rgba(0, 0, 0, 0.12);
+
+          margin-left: 10%;
+        }
+      }
+
+      .product-information-box {
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16),
+          0 2px 10px rgba(0, 0, 0, 0.12);
+        border-radius: 25px;
+        padding: 10px;
+
+        display: grid;
+
+        .product-name {
+          font-weight: bold;
+          text-align: center;
+        }
+      }
+
+      .product-property-box {
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16),
+          0 2px 10px rgba(0, 0, 0, 0.12);
+        justify-content: center;
+        border-radius: 25px;
+
+        display: grid;
+
+        .product-quantity {
+          margin-top: 4rem;
+          align-self: center;
+
+          .info-quant {
+            width: 5rem;
+          }
+        }
+
+        .product-remove {
+          align-self: end;
+          margin-bottom: 10px;
+        }
+      }
+    }
+  }
+}
+
+aside {
+  border-radius: 25px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16), 0 2px 10px rgba(0, 0, 0, 0.12);
+  background-color: whitesmoke;
+  padding: 15px;
+  gap: 1rem;
+
+  grid-template-columns: 1fr;
+  grid-auto-rows: 150px;
+
+  display : table;
+  grid-area: aside;
+
+  .product-information {
+    grid-template-columns: 7fr 3fr;
+    display: grid;
+    gap: 1rem;
+    margin: 10px auto;
+
+    .info {
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16), 0 2px 10px rgba(0, 0, 0, 0.12);
+      border-radius: 25px;
+      background-color: whitesmoke;
+      padding: 10px;
+
+      h1 {
+        font-size: 16px;
+        font-weight: bold;
+      }
+
+      p {
+        text-overflow: ellipsis;
+        word-wrap: break-word;
+        overflow: hidden;
+        max-height: 6rem;
+      }
+    }
+
+    .value {
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16), 0 2px 10px rgba(0, 0, 0, 0.12);
+      border-radius: 25px;
+      background-color: whitesmoke;
+      padding: 10px;
+
+      text-align: center;
+    }
+  }
+
+  .total-value {
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.16), 0 2px 10px rgba(0, 0, 0, 0.12);
+    background-color: rgb(231, 231, 231);
+    border-radius: 50px !important;
+
+    height: 100px;
+    padding: 15px;
+
+    display : table-row;
+    vertical-align : bottom;
+    text-align: center;
+
+    .options {
+      padding: 15px;
+
+      grid-template-columns: 1fr 1fr;
+      gap: 1.5rem;
+
+      display: grid;
+    }
+  }
+}
+</style>
